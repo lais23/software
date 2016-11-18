c42f9536f59ad64db45ea65fa3cd44d6af5c80d9
lais_naihaus <lais_naihaus@SNRSL1ELBIET048>
2016-11-18 09:47:37 -0200

    1 - criação do estilo css 2 - banner 3- criado o menu

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title> Software </title>
+        <link rel="stylesheet" href="css/estilo.css"/>
     </head>
     <body>
+        <div id="container">
+            <div id="header">
+                <img src=""/>
+            </div>
+            <div id="menu">
+                <ul>
+                    <li><a href="?pg=home"> Home </a></li>
+                    <li><a href="?pg=clientes"> Clientes </a></li>
+                    <li><a href="?pg=produtos"> Produtos </a></li>
+                    <li><a href="?pg=servicos"> Serviços </a></li>
+                    <li><a href="?pg=orcamentos"> Orçamentos </a></li>
+                </ul>
+            </div>
+
+        </div>
         <?php
-        // put your code here
+        
         ?>
     </body>
 </html>
